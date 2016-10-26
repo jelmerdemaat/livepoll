3b7525cc895ef9280be502cb612ccefc31e70be0
Jelmer de Maat <post@jelmerdemaat.nl>
2016-10-27 00:58:38 +0200

    Calculate and update average

--- index.js
+++ index.js
@@ -8,18 +8,43 @@ let updateUsers = function() {
 	io.emit('update users', io.engine.clientsCount);
 }
 
+let total, average;
+
+let updateNumbers = function() {
+	total = 0;
+	average = 0;
+
+	for(let client in numbers) {
+		if (!numbers.hasOwnProperty(client)) continue;
+
+		total += numbers[client];
+	}
+
+	average = total / Object.keys(numbers).length;
+
+	console.log('avg: ' + average);
+
+	io.emit('average change', Math.round(average));
+}
+
+let numbers = {};
+
 io.on('connection', function(socket) {
 	updateUsers();
-	console.log('a user connected: ', socket.id);
+	console.log('user connected: ', socket.id);
 
 	socket.on('range change', function(value){
-		console.log('range change: ' + value);
+		console.log('range change: ' + value + ', user: ' + socket.id);
 
-		io.emit('range change', value);
+		numbers[socket.id] = parseInt(value);
+
+		updateNumbers();
 	});
 
-	socket.on('disconnect', function() {
+	socket.on('disconnect', () => {
 		updateUsers();
+		delete numbers[socket.id];
+		updateNumbers();
 		console.log('user disconnected: ', socket.id);
 	});
 });
