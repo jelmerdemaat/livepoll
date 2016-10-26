f7c30b004ef3442f129de84f2ec349eb75fb6d07
Jelmer de Maat <post@jelmerdemaat.nl>
2016-10-26 22:52:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let app = require('express')(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected: ', socket.id);
+
+	socket.on('range change', function(value){
+		console.log('range change: ' + value);
+
+		io.emit('range change', value);
+	});
+
+	socket.on('disconnect', function(){
+		console.log('user disconnected: ', socket.id);
+	});
+});
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
